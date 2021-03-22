c518062e47bbabc66fb123a514907ed9ab330459
ddz <ddzyan@163.com>
2021-03-22 19:49:20 +0800

    完成请求解密

--- test/istring.test.js
+++ test/istring.test.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const Istring = require('../lib');
 
-describe('istring test', function () {
-  const API_KEY = '89561e53-daea-487a-abdf-c32c10298c11';
+describe.skip('istring test', function () {
+  const API_KEY = '89561e53-daea-487a-abdf-c32c10298c12';
   const PLATFORM_PUBLIC_KEY =
-    '-----BEGIN PUBLIC KEY-----\n11IBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiC2KuGa32HtXT5jTygqb/gyP+h2jN7XvbxoTt2IHZvNlv/Kycv2HFYnsgdLOu4tXjUcnFxqXEogMO6CLsWqlrIdRpaQYkr6mt1J8dcwK4sNX6WblFZGg+CDc+AOlkOccRZoCbJ+KzgNIMuDRiMFFqlG/bBolofHtjEwOSCZqjLOWKNK0Evd9QPFn2ub0ptUuZQyB/sNH4DGYgYdT/g2tbp5ZfPnFuD2BV2xJ7PathosJT2qQFbOkYbibnMaNoXYJvXjDPATsShOiG9uZtQa8UXDCtZM08+g4G1iJGTt4TzW7jv59a/kiCmogNKBZ/HzWfeWxJLlhry9FQi9EJZ+jZwIDAQAB\n-----END PUBLIC KEY-----\n';
+    '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiC2KuGa32HtXT5jTygqb/gyP+h2jN7XvbxoTt2IHZvNlv/Kycv2HFYnsgdLOu4tXjUcnFxqXEogMO6CLsWqlrIdRpaQYkr6mt1J8dcwK4sNX6WblFZGg+CDc+AOlkOccRZoCbJ+KzgNIMuDRiMFFqlG/bBolofHtjEwOSCZqjLOWKNK0Evd9QPFn2ub0ptUuZQyB/sNH4DGYgYdT/g2tbp5ZfPnFuD2BV2xJ7PathosJT2qQFbOkYbibnMaNoXYJvXjDPATsShOiG9uZtQa8UXDCtZM08+g4G1iJGTt4TzW7jv59a/kiCmogNKBZ/HzWfeWxJLlhry9FQi9EJZ+jZwIDAQAB\n-----END PUBLIC KEY-----\n';
   const CHAR_ENCODE_UTF_8 = 'utf8';
 
   const privatePath = path.join(__dirname, '../pem/istring_private.pem');
@@ -18,9 +18,10 @@ describe('istring test', function () {
   });
 
   it('create address test', async function () {
-    const res = await istring.createAddress('ETH', 1);
-    const { code, data } = res;
-    assert.strictEqual(code, '20000', '状态码错误');
+    const res = await istring.createAddress('TRX', 1);
+    console.log(res);
+    const { code, data, errMsg } = res;
+    assert.strictEqual(code, '20000', errMsg);
     assert.ok(data.length > 0, '返回数据错误');
   });
 });
--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
-
+const fs = require('fs');
+const path = require('path');
 const util = require('../lib/util');
 
 const PLATFORM_PUBLIC_KEY =
@@ -8,6 +9,7 @@ const PLATFORM_PUBLIC_KEY =
 describe('util test', function () {
   let randomStr = '';
   let key = '';
+  let aesSaltDecrypt;
   it('aesEncryptByECBPKCS7Padding test', function () {
     const str = util.aesEncryptByECBPKCS7Padding(
       'this is a test message',
@@ -30,5 +32,42 @@ describe('util test', function () {
   it('rsaEncrypt test', function () {
     const data = util.rsaEncrypt('tbDAddCtfDReZ7HdD2crQw==', PLATFORM_PUBLIC_KEY);
     assert.strictEqual(data.length, 344, '长度错误');
+  });
+
+  it('rsaDecrypt test', function () {
+    const privatePath = path.join(__dirname, '../pem/istring_private.pem');
+    const userPrivateKey = fs.readFileSync(privatePath);
+    const data = util.rsaDecrypt(
+      'CYAtrtK77z7qucHOVmsUWTA6fGvW1Cl0oDC7cKcGGOcRvssFOVnZ1qQeMyDxDLHZx0ZTvfxAVT07hgnWIHlE4QmC+MC9sCBFgCs1yhC/jfrNvgq2R5/KK9ZMVhBWAo6k4afY2vXTEYfQSYJcjVgjF7GvwY4KgaFMKtJU22R3QuUbANd8zTL6MVbnDYxnc1WkDjwTKELa0gp97fyEcGFaGAJUuNySW6JJYfbl2NsLtlkWiSm+OeGhlKDB55LGNJjXM0aoqrFkmWsJimZqKvQvMDkpaDkJSoVj+q61ZPJNcDYRTwJXZDz+2n8gQXG0ZS0VXC1/JmAUVvs3dmILdCu1Eg====',
+      userPrivateKey
+    );
+    assert.strictEqual(data.length, 24, '长度错误');
+  });
+
+  it('rsaDecrypt test', function () {
+    const privatePath = path.join(__dirname, '../pem/istring_private.pem');
+    const userPrivateKey = fs.readFileSync(privatePath);
+    const data = util.rsaDecrypt(
+      'CYAtrtK77z7qucHOVmsUWTA6fGvW1Cl0oDC7cKcGGOcRvssFOVnZ1qQeMyDxDLHZx0ZTvfxAVT07hgnWIHlE4QmC+MC9sCBFgCs1yhC/jfrNvgq2R5/KK9ZMVhBWAo6k4afY2vXTEYfQSYJcjVgjF7GvwY4KgaFMKtJU22R3QuUbANd8zTL6MVbnDYxnc1WkDjwTKELa0gp97fyEcGFaGAJUuNySW6JJYfbl2NsLtlkWiSm+OeGhlKDB55LGNJjXM0aoqrFkmWsJimZqKvQvMDkpaDkJSoVj+q61ZPJNcDYRTwJXZDz+2n8gQXG0ZS0VXC1/JmAUVvs3dmILdCu1Eg====',
+      userPrivateKey
+    );
+    aesSaltDecrypt = data;
+    assert.strictEqual(data.length, 24, '长度错误');
+  });
+
+  it('aesDecryptByECBPKCS7Padding test', function () {
+    const data = util.aesDecryptByECBPKCS7Padding(
+      'ZMGB6qwZpW6VpLYQpzylj4Y7bW086k7e5wIFlha3al904YMe+N2i12bSgVwb2ZQE55yOvvcDHUmouiXB4R2YBga+PfXmkHpCoMjnS5hwhecfMpQRQRabocGoGVBXlPWa5j2BnJve7aa5VqbG8oL8E2uC0PHqNqWt6wEfWYvgSIFxlBlaYLjm9k2PwSw0RyRkdMt+rmC5f9x64tv1y9fheKkeZquPxjiMx8TZnN/Jdv7BwKOR+QNUKMhrI5zdFPZ3ahE/6at2ACaZkhFW/+tzqyAunoqgl6kzxvzMYq6C8hqao3YlnqHvwulpsn2yh76DOFuqAt4TWigyDOt8/j9FAa2tG7sfe7EeNIvKQ7JfM5zIWrLDuD5AYImM638fnvG8pgTAHynF2xmzM+EHqKfls+S83EVMWB8Z3r3cNBLe5iOt1ikfMQnIGTIXaB39QyeoCZVljVJPFxHyAkE+dBzftpD1C6EIsB1ZKX5kjtNhUOWo7fPbZMK9Q0CxEoUDkdmuWyVTVS5rTQ+v4rj3axf5hscRhqV6u7c4ndWMt/ELFsgwaoEZXm6FOZUqQdn6YMSy9ReHGzmce+wWuMv99keQ0QQVDtCTwRJ7cJ+MlxLUw7dQu70GeU0JXfRIYLrWzaUXVVD+Xr+8UqWLhz+lNTXVAQ==',
+      aesSaltDecrypt
+    );
+    const requestObj = JSON.parse(data);
+    assert.ok(requestObj.uniqTradeNo, 'uniqTradeNo 不存在');
+    assert.ok(requestObj.outIndex, 'outIndex 不存在');
+    assert.ok(requestObj.tradeNo, 'tradeNo 不存在');
+    assert.ok(requestObj.tradeFee, 'tradeFee 不存在');
+    assert.ok(requestObj.toAddress, 'toAddress 不存在');
+    assert.ok(requestObj.tradeAmount, 'tradeAmount 不存在');
+    assert.ok(requestObj.fromAddress, 'fromAddress 不存在');
+    assert.ok(requestObj.toAddress, 'toAddress 不存在');
   });
 });
